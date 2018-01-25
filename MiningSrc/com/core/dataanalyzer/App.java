e00aacfaebd6fce7842e92fbc1b575baf7f6de64
lkanev <lubokkanev@gmail.com>
2018-01-25 21:55:07 +0200

    remove redundant code

--- MiningSrc/com/core/dataanalyzer/App.java
+++ MiningSrc/com/core/dataanalyzer/App.java
@@ -1,41 +1,11 @@
 package com.core.dataanalyzer;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.misc.HighFreqTerms;
-import org.apache.lucene.misc.TermStats;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.RAMDirectory;
 
 import com.core.webcrawler.impl.Spider;
 import com.datumbox.opensource.classifiers.NaiveBayes;
@@ -43,145 +13,9 @@ import com.datumbox.opensource.dataobjects.NaiveBayesKnowledgeBase;
 
 
 public class App {
-   private static final String FULLPATH_FIELD = "fullpath";
-   private static final String FILENAME_FIELD = "filename";
-   private static final String CONTENTS_FIELD = "contents";
-   static Spider testSpider;
-   private static final int HITS_LIMIT = 10;
+   private static Spider testSpider;
 
-   private static IndexWriter createIndex(Directory index, Analyzer analyzer)
-         throws IOException {
-      IndexWriterConfig config = new IndexWriterConfig(analyzer);
-
-      return new IndexWriter(index, config);
-   }
-
-   private static Analyzer createDefaultAnalyzer() {
-      Map<String, Analyzer> analyzerMap = new HashMap<>();
-      analyzerMap.put(CONTENTS_FIELD, new StandardAnalyzer());
-
-      return new PerFieldAnalyzerWrapper(new StandardAnalyzer(), analyzerMap);
-   }
-
-   private static Document createDocument(File f) {
-      // FULLPATH_FIELD (string), FILENAME_FIELD (string), CONTENTS_FIELD (text)
-
-      Document doc = new Document();
-      try {
-         FileReader fileReader = new FileReader(f);
-         BufferedReader bufferedReader = new BufferedReader(fileReader);
-         doc.add(new StringField(FULLPATH_FIELD, f.getAbsolutePath(),
-               Field.Store.YES));
-         doc.add(new StringField(FILENAME_FIELD, f.getName(), Field.Store.YES));
-         doc.add(new TextField(CONTENTS_FIELD, new FileReader(f)));
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-
-      return doc;
-   }
-
-   private static void loadData(Directory index, String path, Analyzer analyzer)
-         throws IOException {
-      try (IndexWriter w = createIndex(index, analyzer)) {
-         Files.walk(Paths.get(path))
-               .parallel()
-               .filter(f -> Files.isRegularFile(f))
-               .map(Path::toFile)
-               .map(App::createDocument)
-               .filter(Objects::nonNull)
-               .forEach(doc -> {
-                  System.out.println(doc.get(FULLPATH_FIELD));
-                  // TODO add document to the store through the IndexWriter object
-                  try {
-                     w.addDocument(doc);
-                  } catch (IOException e) {
-                     e.printStackTrace();
-                  }
-               });
-
-         System.out.println("Loaded documents " + w.numDocs());
-      }
-   }
-
-   private static ScoreDoc[] getScoredDocs(Query q, int hitsLimit,
-         IndexSearcher searcher) throws IOException {
-      // TODO get top documents and extract ScoreDocs[] from them
-      return searcher.search(q, hitsLimit).scoreDocs;
-   }
-
-   private static Query buildQuery(String querystr, Analyzer analyzer)
-         throws ParseException {
-      // TODO construct query object based on CONTENTS_FIELD
-      return new QueryParser(CONTENTS_FIELD, analyzer).parse(querystr);
-   }
-
-   private static IndexSearcher createSearcher(IndexReader reader) {
-      IndexSearcher searcher = new IndexSearcher(reader);
-      // TODO set similarity to the searcher
-      searcher.setSimilarity(IndexSearcher.getDefaultSimilarity());
-
-      return searcher;
-   }
-
-   private static void search(Directory index, Query q, boolean verbose)
-         throws IOException {
-      // 3. search
-
-      // reader can only be closed when there
-      // is no need to access the documents any more.
-      try (IndexReader reader = DirectoryReader.open(index)) {
-         IndexSearcher searcher = createSearcher(reader);
-
-         ScoreDoc[] hits = getScoredDocs(q, HITS_LIMIT, searcher);
-
-         // 4. display results
-         System.out.println("Found " + hits.length + " hits.");
-
-         for (int i = 0; i < hits.length; ++i) {
-            // TODO Extract docId and score from hits object
-            int docId = hits[i].doc;
-            double score = hits[i].score;
-
-            Document d = searcher.doc(docId);
-
-            String output = String.format("%d.\t%d\t%f\t%s", (i + 1), docId,
-                  score, d.get(FULLPATH_FIELD));
-
-            System.out.println(output);
-            if (verbose) {
-               System.out.println("\nExplanation of hit score calculation: ");
-               System.out.println(searcher.explain(q, docId));
-            }
-            System.out.println(
-                  "----------------------------------------------------------------");
-         }
-
-      }
-   }
-
-   public static void main(String[] args) throws IOException {
-      Directory index = new RAMDirectory();
-      String articlesPath =
-            "C:\\Users\\traychev\\Documents\\workspace-sts-3.8.3.RELEASE\\TopicClassifierDataMining\\articles";
-      Analyzer analyzer = createDefaultAnalyzer();
-      loadData(index, articlesPath, analyzer);
-
-      try {
-         IndexWriter writer =
-               new IndexWriter(FSDirectory.open(new File(articlesPath)
-                     .toPath()), new IndexWriterConfig(analyzer));
-         IndexReader reader = DirectoryReader.open(writer, true, true);
-         TermStats[] commonTerms = HighFreqTerms.getHighFreqTerms(reader, 30,
-               null, new HighFreqTerms.DocFreqComparator());
-         for (TermStats commonTerm : commonTerms) {
-            System.out.println(commonTerm.termtext.utf8ToString());
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+   public static void main(String[] args) {
       HashMap<String, IndexContainerImpl> forBayesRaychev = new HashMap<>();
       String[] topics = new String[] { "sport", "science" };
       for (String topic : topics) {
@@ -244,7 +78,7 @@ public class App {
       } catch (Throwable e) {
          //?
       }
-      Map<String, String[]> tokens2 = new HashMap();
+      Map<String, String[]> tokens2 = new HashMap<>();
       for (Map.Entry<String, String[]> entry : tokens.entrySet()) {
          tokens2.put(entry.getKey(), new String[] { String.join(" ", entry
                .getValue()) });
@@ -261,17 +95,17 @@ public class App {
    }
 }
 
-class StrictTokenAnalyzerImpl extends Object {
+class StrictTokenAnalyzerImpl {
    public String word;
-   public int occurance;
+   public int occurrence;
 
    public StrictTokenAnalyzerImpl(String word, int occurrence) {
       this.word = word;
-      this.occurance = occurrence;
+      this.occurrence = occurrence;
    }
 }
 
-class IndexContainerImpl extends Object {
+class IndexContainerImpl {
    public ArrayList<StrictTokenAnalyzerImpl> strictTokenAnalyzerImpls;
 
    public IndexContainerImpl(
@@ -284,21 +118,14 @@ class IndexContainerImpl extends Object {
    }
 
    public void sort() {
-      strictTokenAnalyzerImpls.sort((o1, o2) -> {
-         if (o1.occurance < o2.occurance)
-            return 1;
-         else if (o1.occurance > o2.occurance)
-            return -1;
-         else
-            return 0;
-      });
+      strictTokenAnalyzerImpls.sort((o1, o2) -> Integer.compare(o2.occurrence, o1.occurrence));
    }
 
    public void print() {
       for (StrictTokenAnalyzerImpl strictTokenAnalyzerImpl : strictTokenAnalyzerImpls) {
          System.out.println(
                "The word \"" + strictTokenAnalyzerImpl.word + "\" is found "
-                     + strictTokenAnalyzerImpl.occurance
+                     + strictTokenAnalyzerImpl.occurrence
                      + " times");
       }
    }
--- MiningSrc/com/core/dataanalyzer/LuceneInvertedIndex.java
+++ MiningSrc/com/core/dataanalyzer/LuceneInvertedIndex.java
@@ -18,10 +18,10 @@ public class LuceneInvertedIndex {
    List<String> files = new ArrayList<String>();
 
    public void indexFile(File file) throws IOException {
-      int fileno = files.indexOf(file.getPath());
-      if (fileno == -1) {
+      int fileNumber = files.indexOf(file.getPath());
+      if (fileNumber == -1) {
          files.add(file.getPath());
-         fileno = files.size() - 1;
+         fileNumber = files.size() - 1;
       }
 
       int pos = 0;
@@ -35,10 +35,10 @@ public class LuceneInvertedIndex {
                continue;
             List<Tuple> idx = index.get(word);
             if (idx == null) {
-               idx = new LinkedList<Tuple>();
+               idx = new LinkedList<>();
                index.put(word, idx);
             }
-            idx.add(new Tuple(fileno, pos));
+            idx.add(new Tuple(fileNumber, pos));
          }
       }
       System.out.println("indexed " + file.getPath() + " " + pos + " words");
@@ -72,5 +72,5 @@ public class LuceneInvertedIndex {
       }
    }
 
-   /* java -cp bin org.traychev&lkanev*lkanev^2.InvertedIndex "huntsman,merit,dog,the,gutenberg,lovecraft,olympian" lakenv.txt pg7025.txt pg82.txt pg9090.tx. */List<String> stopwords = Arrays.asList("a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your");
+   /* java -cp bin org.traychev&lkanev^2.InvertedIndex "huntsman,merit,dog,the,gutenberg,lovecraft,olympian" lakenv.txt pg7025.txt pg82.txt pg9090.tx. */List<String> stopwords = Arrays.asList("a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your");
 }
